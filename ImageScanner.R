c243cce8d6d027ac771c59aacc27c4d604aec10c
Ariel Levi Simons <levisimons@gmail.com>
2018-10-20 15:13:05 -0700

    Create ImageScanner.R

---
+++ ImageScanner.R
@@ -0,0 +1,24 @@
+library(hexView)
+library(broman)
+
+setwd("~/Desktop/Coastlight")
+
+directories <- list.dirs('.', recursive=FALSE)
+
+imageSaturations <- data.frame()
+
+for(directory in directories){
+  fileNames <- list.files(path=directory)
+  for(fileName in fileNames){
+    imageFile <- paste(directory,fileName,sep="/")
+    rawImage <- readRaw(imageFile) #Read .CR2 raw images as a rawBlock.
+    saturationCount <- sum(hex2dec(rawImage$fileRaw)==255) #Count the number of saturated pixels (value=255)
+    row <- data.frame()
+    row[1,1] <- fileName
+    row[1,2] <- saturationCount
+    imageSaturations <- rbind(imageSaturations,row)
+    print(nrow(imageSaturations))
+  }
+}
+colnames(imageSaturations) <- c("Filename","SaturatedPixelCount")
+write.table(imageSaturations,"imageSaturations.txt",quote=FALSE,sep="\t",row.names = FALSE)
