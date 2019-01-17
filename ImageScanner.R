5b653b8afe11d72b882f780319f867afe96c6f0e
Ariel Levi Simons <levisimons@gmail.com>
2019-01-17 15:38:33 -0800

    Update ImageScanner.R

--- ImageScanner.R
+++ ImageScanner.R
@@ -1,19 +1,18 @@
 ##This script counts all of the saturated pixels in the raw image files.
 ##These counts are used to determine which image exposure produces the fewest saturated pixels for downstream analysis.
-library(hexView)
-library(broman)
 library(plyr)
 library(ggplot2)
 library(jpeg)
+library(dplyr)
 
-wd <- "~/Desktop/Coastlight"
+wd <- "~/Desktop/Coastlight/tmp"
 setwd(wd)
 
 ## Code for generating the saturation pixel count per image:
 # If your images are in CR2 format please first convert them to jpg format.
 # Unix command for this conversion:
 # for d in ./*/ ; do (cd "$d" && for i in *.CR2; do sips -s format jpeg $i --out "${i%.*}.jpg"; done); done
-
+# If in a single directory, to convert CR2 files to jpg just run: for d in ./*/ ; do (for i in *.CR2; do sips -s format jpeg $i --out "${i%.*}.jpg"; done); done
 directories <- list.dirs('.', recursive=FALSE)
 
 imageSaturations <- data.frame()
@@ -45,3 +44,11 @@ imageSaturations <- read.table("imageSaturations.txt",header=TRUE, sep="\t",as.i
 coastLightData <- read.table("CoastlightData.tsv",header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
 
 CoastlightMerged <-  join(coastLightData,imageSaturations,by=c("Filename"))
+
+CoastlightMerged$SaturatedPixelPercentage <- CoastlightMerged$SaturatedPixelCount/2.4e6
+
+CoastlightMerged$Date <- as.Date(CoastlightMerged$Date, "%m/%d/%y")
+
+CoastlightMerged <- CoastlightMerged[order(CoastlightMerged$Date,CoastlightMerged$Time,CoastlightMerged$SQCSiteName,CoastlightMerged$`Exposure time (s)`,-CoastlightMerged$SaturatedPixelPercentage),]
+
+write.table(CoastlightMerged,"CoastlightSaturations.txt",quote=FALSE,sep="\t",row.names = FALSE)
