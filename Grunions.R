a473ee967cf9b8d73b84b204555275e3cdf030c7
Ariel Levi Simons <levisimons@gmail.com>
2019-05-08 13:30:46 -0700

    Update Grunions.R

--- Grunions.R
+++ Grunions.R
@@ -1,6 +1,13 @@
 require(dplyr)
+require(ggplot2)
+require(leaflet)
+require(mapview)
+require(sp)
+require(viridis)
 ##This script reads in monthly grunion spawning data and finds the spawning event for a given
 ##site which has the highest Walker scale value for the year.
+##It then maps, by year, the beach areas of each grunion run and color codes them by
+##their maximum annual Walker score.
 
 wd <- "~/Desktop/Coastlight/Grunions"
 setwd(wd)
@@ -16,16 +23,45 @@ for(grunionFile in grunionFiles){
   grunionMax <- aggregate(Walker ~ YYYY, data = grunionSite, max)
   #Order grunion maximum spawn events by year
   grunionMax <- arrange(grunionMax,YYYY)
+  colnames(grunionMax)[which(names(grunionMax) == "YYYY")] <- "Year"
   #Get site name
-  siteName <- unlist(strsplit(grunionFile,split='_',fixed=TRUE))[1]
-  #Rename maximum spawn column.
-  colnames(grunionMax)[which(names(grunionMax) == "Walker")] <- paste("MaxWalker",siteName,sep="")
+  siteName <- unlist(strsplit(grunionFile,split='_monthly',fixed=TRUE))[1]
+  grunionMax$SiteName <- siteName
   #Combine all of the max spawn events by site and year into a single data frame.
-  if(ncol(allSitesGrunionMax) == 0){
-    allSitesGrunionMax <- grunionMax
-  } else{
-    allSitesGrunionMax <- cbind(allSitesGrunionMax,grunionMax)
-  }
+  allSitesGrunionMax <- rbind(allSitesGrunionMax,grunionMax)
 }
-#Remove duplicated year column from max spawn events by site and year data frame
-allSitesGrunionMax <- allSitesGrunionMax[,!duplicated(colnames(allSitesGrunionMax))]
+#Read in gps coordinates of the bounding boxes denoting each beach site.
+BoundaryInput <- read.delim("Bounding_Boxes.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE, encoding = "UTF-8")
+
+#Merge in geographic data into grunion run data.
+allSitesGrunionMaxGPS <- merge(allSitesGrunionMax,BoundaryInput,by.x=c("SiteName"),by.y=c("Bounding_Box"))
+
+#Subset grunion data by year for mapping.
+GivenYear <- 2003 #Between 2003 and 2018
+allSitesGrunionMaxGPSYear <- subset(allSitesGrunionMaxGPS,Year==GivenYear)
+
+#Map grunion runs.
+MapCoordinates <- allSitesGrunionMaxGPSYear
+#Set centroid coordinates of each beach area.
+MapCoordinates$lat <- 0.5*(MapCoordinates$lat_min+MapCoordinates$lat_max)
+MapCoordinates$lon <- 0.5*(MapCoordinates$lon_min+MapCoordinates$lon_max)
+#Map data.
+dev.off()
+#Color code beach areas by the maximum annual Walker score.
+ColorScale <- colorNumeric(palette=plasma(10),domain=allSitesGrunionMaxGPS$Walker)
+CalMap = leaflet(MapCoordinates) %>% 
+  addTiles() %>%
+  addProviderTiles(providers$Esri.WorldTopoMap) %>%
+  setView(lng=mean(MapCoordinates$lon),lat=mean(MapCoordinates$lat),zoom=6) %>%
+  addLegend("topright", pal=ColorScale,values=~Walker,title=paste("Max Walker scale<br>Year",GivenYear))
+
+CalMap
+for(site in unique(MapCoordinates$SiteName)){
+  lon_min=MapCoordinates[MapCoordinates$SiteName==site,"lon_min"]
+  lon_max=MapCoordinates[MapCoordinates$SiteName==site,"lon_max"]
+  lat_min=MapCoordinates[MapCoordinates$SiteName==site,"lat_min"]
+  lat_max=MapCoordinates[MapCoordinates$SiteName==site,"lat_max"]
+  MaxWalker=MapCoordinates[MapCoordinates$SiteName==site,"Walker"]
+  CalMap <- CalMap %>% addRectangles(lng1=lon_min,lat1=lat_min,lng2=lon_max,lat2=lat_max,color=~ColorScale(MaxWalker),weight=5,fillColor="transparent")
+}
+CalMap
