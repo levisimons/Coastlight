f5469a0522b577afd31804cc44fe37e6aef1d55f
Ariel Levi Simons <levisimons@gmail.com>
2019-05-16 18:05:22 -0700

    Create LIDARCleaner.sh

---
+++ LIDARCleaner.sh
@@ -0,0 +1,8 @@
+# This script will run gdalwarp on each LIDAR tif file from NOAA and create a cleaned version with erroneous values removed.
+for f in *.tif
+do
+        inputFile="${f}"
+        outputFile="${f/%.tif/_cleaned.tif}"
+        echo "${inputFile} ${outputFile}"
+        gdalwarp -of ENVI -dstnodata -999999 $inputFile $outputFile
+done
