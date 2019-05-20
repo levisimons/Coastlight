7388b305e95f0bd35f1b3eb6ba4bb681bb53a147
Ariel Levi Simons <levisimons@gmail.com>
2019-05-20 12:37:50 -0700

    Update DEMMerger.sh

--- DEMMerger.sh
+++ DEMMerger.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem=1200000m
 #SBATCH -pSCCWRP
 cd /home/cmb-07/sn1/alsimons/Coastlight
-gdalbuildvrt mergedCoast.vrt /home/cmb-07/sn1/alsimons/Coastlight/*_compressed.tif
+gdalbuildvrt mergedCoast.vrt /home/cmb-07/sn1/alsimons/Coastlight/Job470138_west_coast_2016_el_nino_dem_m6260_*.tif
 gdal_translate -of GTiff -co compress=lzw mergedCoast.vrt mergedCoast.tif 
