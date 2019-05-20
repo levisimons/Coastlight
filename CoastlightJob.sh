32627d55744473c45d89ec4ae5937eed1f3694e5
Ariel Levi Simons <levisimons@gmail.com>
2019-05-19 20:44:14 -0700

    Update CoastlightJob.sh

--- CoastlightJob.sh
+++ CoastlightJob.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH -t100:00:00
+#SBATCH -t300:00:00
 #SBATCH -n1
 #SBATCH -c1
-#SBATCH --mem=1000000m
+#SBATCH --mem=1200000m
 #SBATCH -pSCCWRP
 cd /home/cmb-07/sn1/alsimons/Coastlight
